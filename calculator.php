5991320bd07b132f629c2bc130596b21340321b0
Filip911 <57196505+Filip911@users.noreply.github.com>
2019-11-18 16:48:24 +0100

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+<title>Calculator</title>	
+</head>
+
+<body>
+
+<form>
+	<input type="text" name="num1" placeholder="number1">
+	<input type="text" name="num2" placeholder="number2">
+	<select name="operator">
+		<option>None</option>
+		<option>Add</option>
+		<option>Subtract</option>
+		<option>Multyply</option>
+		<option>Divide</option>
+	</select>
+	<br>
+	<button type="submit" name="submit" value="submit">Calculate</button>
+</form>
+<p>The answer is:</p>
+<?php
+	if (isset($_GET['submit'])) {
+		$result1 = $_GET['num1'];
+		$result2 = $_GET['num2'];
+		$operator = $_GET['operator'];
+		switch ($operator) {
+			case "None":
+				echo "Selecet a method!";
+			break;
+			case "Add":
+				echo $result1 + $result2;
+			break;
+			case "Subtract":
+				echo $result1 - $result2;
+			break;
+			case "Multyply":
+				echo $result1 * $result2;
+			break;
+			case "Divide":
+				echo $result1 / $result2;
+			break;
+		}
+	}
+
+
+?>
+
+
+</body>
+
+</html>
