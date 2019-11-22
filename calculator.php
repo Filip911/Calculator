6fd432654d255adc67097b398e8f06897beadae2
Filip911 <filipworkrate@gmail.com>
2019-11-22 15:05:48 +0100

    modify

--- calculator.php
+++ calculator.php
@@ -1,56 +1,50 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-<title>Calculator</title>
-</head>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-<body>
-
-	<p>
-
-	<form>
-	<input type="text" name="num1" placeholder="number1">
-	<input type="text" name="num2" placeholder="number2">
-	<select name="operator">
-		<option>None</option>
-		<option>Add</option>
-		<option>Subtract</option>
-		<option>Multyply</option>
-		<option>Divide\</option>
-	</select>
-	<br>
-	<button type="submit" name="submit" value="submit">Calculate</button>
-</form>
-<p>The answer is:</p>
+    <form>
+      <input type="text" name="number1" placeholder="number1" />
+      <input type="text" name="number2" placeholder="number2" />
+      <select name="operator">
+        <option>NONE</option>
+        <option>+</option>
+        <option>-</option>
+        <option>*</option>
+        <option>/</option>
+      </select>
+      <br>
+      <button type="submit" name="submit" value="submit">Calculate</button>
+    </form>
 <?php
-	if (isset($_GET['submit'])) {
-		$result1 = $_GET['num1'];
-		$result2 = $_GET['num2'];
-		$operator = $_GET['operator'];
-		switch ($operator) {
-			case "None":
-				echo "Selecet a method!";
-			break;
-			case "Add":
-				echo $result1 + $result2;
-			break;
-			case "Subtract":
-				echo $result1 - $result2;
-			break;
-			case "Multyply":
-				echo $result1 * $result2;
-			break;
-			case "Divide":
-				echo $result1 / $result2;
-			break;
-		}
-	}
+    if (isset($_GET['submit'])) {
+      $result1 = $_GET['number1'];
+      $result2 = $_GET['number2'];
+      $operator = $_GET['operator'];
+    switch ($operator) {
+      case "None":
+        echo "Error";
+        break;
+      case "+":
+        echo $result1 + $result2;
+        break;
+      case "-":
+        echo $result1 - $result2;
+        break;
+      case "*":
+        echo $result1 * $result2;
+        break;
+      case "/":
+        echo $result1 / $result2;
+        break;
+      }
+    }
 
-<p>haha</p>
-?>
+ ?>
+<p>ja</p>
 
-
-</body>
-
+  </body>
 </html>
